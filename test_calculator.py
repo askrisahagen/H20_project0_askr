5ac4198ee6f8d29706b64ac3278c4e979b626fa8
Ask Risahagen <askr@student.matnat.uio.no>
2020-08-29 13:49:19 +0200

    "Made a change, added test for string"

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,12 @@
 import calculator
 
-def test_add():
+def test_add_integer():
     assert calculator.add(1,2) == 3
 
 def test_add_float():
     from math import isclose
 
     assert isclose(calculator.add(0.1,0.2), 0.3)
+
+def test_add_string():
+    assert calculator.add("Hello ","World") == "Hello World"
