3f8fb9673a9969f6028b462ac0d5e3dddf64491c
Ask Risahagen <askr@student.matnat.uio.no>
2020-08-30 23:51:01 +0200

    Paramertrized all tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,58 +1,53 @@
 import calculator
+import pytest
+import math
 
-def test_add_integer():
-    assert calculator.add(1,2) == 3
+@pytest.mark.parametrize("arg, expected_output", [[(-1, -1), -2], [(1, 1), 2], [(1, 0), 1]])
+def test_add_integer(arg, expected_output):
+    assert calculator.add(arg[0], arg[1]) == expected_output
 
-def test_add_float():
-    from math import isclose
+@pytest.mark.parametrize("arg, expected_output", [[(-1.1, -1.2), -2.3], [(1.4, 1.5), 2.9], [(1.0, 0.1), 1.1]])
+def test_add_float(arg, expected_output):
+    assert math.isclose(calculator.add(arg[0], arg[1]), expected_output)
 
-    assert isclose(calculator.add(0.1,0.2), 0.3)
+@pytest.mark.parametrize("arg, expected_output", [[("Hello ", "world"), "Hello world"], [("To ", "you"), "To you"], [("Git", "Hub"), "GitHub"]])
+def test_add_string(arg, expected_output):
+    assert calculator.add(arg[0], arg[1]) == expected_output
 
-def test_add_string():
-    assert calculator.add("Hello ","World") == "Hello World"
+@pytest.mark.parametrize("arg, expected_output", [(4, 24), (0, 1), (3,6), (-1, "This factorial only works with integers")])
+def test_factorial_integer(arg, expected_output):
+    assert calculator.factorial(arg) == expected_output
 
-def test_factorial_integer():
-    assert calculator.factorial(4) == 24
+@pytest.mark.parametrize("arg, expected_output", [[(math.pi/3, 5), 3**0.5/2], [(math.pi/6, 4), 0.5], [(math.pi/2, 6), 1]])
+def test_sin(arg, expected_output):
+    assert math.isclose(calculator.sin(arg[0], arg[1]), expected_output)
 
-def test_factorial_negativ():
-    assert calculator.factorial(-1) == "This factorial only works with integers"
+@pytest.mark.parametrize("arg, expected_output",[[(-1, -1), 1], [(1, 1), 1], [(6, 2), 3]])
+def test_divide_integers(arg, expected_output):
+    assert calculator.divide(arg[0], arg[1]) == expected_output
 
-def test_factorial_zero():
-    assert calculator.factorial(0) == 1
+@pytest.mark.parametrize("arg, expected_output",[[(-1.2, -1.2), 1.0], [(1.5, 3), 0.5], [(6.9, 3), 2.3]])
+def test_divide_floats(arg, expected_output):
+    assert math.isclose(calculator.divide(arg[0], arg[1]), expected_output)
 
-def test_sin():
-    import math
+@pytest.mark.parametrize("arg, expected_output",[(-3,3), (2,2), (-5,5)])
+def test_abs_integer(arg, expected_output):
+    assert calculator.abs(arg) == expected_output
 
-    assert math.isclose(calculator.sin(math.pi/3,5), 3**0.5/2)
+@pytest.mark.parametrize("arg, expected_output",[(-3.3,3.3), (2.4,2.4), (-5.6,5.6)])
+def test_abs_float(arg, expected_output):
+    assert math.isclose(calculator.abs(arg), expected_output)
 
-def test_divide_integers():
-    assert calculator.divide(12,-3) == -4
-
-def test_divide_floats():
-    from math import isclose
-
-    assert isclose(calculator.divide(0.4,0.1), 4.0)
-
-def test_abs_integer():
-    assert calculator.abs(-3) == 3
-
-def test_abs_float():
-    from math import isclose
-
-    assert isclose(calculator.abs(20.2),20.2)
-
-def test_nthroot():
+@pytest.mark.parametrize("arg, expected_output",[[(27, 3), 3], [(4, 2), 2], [(125, 3), 5]])
+def test_nthroot(arg, expected_output):
     assert calculator.nthroot(27,3) == 3 
 
-def test_add_raises_TypeError():
-    import pytest
-
+@pytest.mark.parametrize("arg",[[2, "you"], ["Pie ", 3], [2, "me"]])
+def test_add_raises_TypeError(arg):
     with pytest.raises(TypeError):
-        calculator.add(2,"you")
+        calculator.add(arg[0],arg[1])
 
-def test_divide_raises_ZeroDivision_Error():
-    import pytest
-
+@pytest.mark.parametrize("arg",[[2, 0], [12392781, 0], [23467, 0]])
+def test_divide_raises_ZeroDivision_Error(arg):
     with pytest.raises(ZeroDivisionError):
-        calculator.divide(4,0)
-
+        calculator.divide(arg[0],arg[1])
